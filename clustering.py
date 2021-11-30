251a26bd3e78ed321d7778db5936c019697f09d9
William Armstrong <71415525+wxarmstrong@users.noreply.github.com>
2021-11-30 06:33:00 -0800

    Add files via upload

--- clustering.py
+++ clustering.py
@@ -18,26 +18,49 @@ from sklearn import metrics
 df = pd.read_csv('training_data.csv', sep=',', header=None) #reading the data by using Pandas library
 
 #assign your training data to X_training feature matrix
+X_training = df
 
 #run kmeans testing different k values from 2 until 20 clusters
      #Use:  kmeans = KMeans(n_clusters=k, random_state=0)
      #      kmeans.fit(X_training)
      #--> add your Python code
+     
+max_s = 0
+max_k = 0
 
+xpoints = []
+ypoints = []
+     
+for k in range(2,20):
+    kmeans = KMeans(n_clusters=k, random_state=0)
+    kmeans.fit(X_training)
 
      #for each k, calculate the silhouette_coefficient by using: silhouette_score(X_training, kmeans.labels_)
      #find which k maximizes the silhouette_coefficient
      #--> add your Python code here
+    s = silhouette_score(X_training, kmeans.labels_)
+    if (s > max_s):
+        max_s = s
+        max_k = k
+    xpoints.append(k)
+    ypoints.append(s)
+    
 
 #plot the value of the silhouette_coefficient for each k value of kmeans so that we can see the best k
 #--> add your Python code here
+a = plt.figure()
+axes = a.add_axes([0.1,0.1,0.8,0.8])
+axes.plot(xpoints, ypoints)
+plt.show()
 
 #reading the validation data (clusters) by using Pandas library
 #--> add your Python code here
+td = pd.read_csv('testing_data.csv', sep=',', header=None)
 
 #assign your data labels to vector labels (you might need to reshape the row vector to a column vector)
 # do this: np.array(df.values).reshape(1,<number of samples>)[0]
 #--> add your Python code here
+labels = np.array(td.values).reshape(1,3823)[0]
 
 #Calculate and print the Homogeneity of this kmeans clustering
 print("K-Means Homogeneity Score = " + metrics.homogeneity_score(labels, kmeans.labels_).__str__())
@@ -47,6 +70,8 @@ print("K-Means Homogeneity Score = " + metrics.homogeneity_score(labels, kmeans.
 #Do it:
 #agg = AgglomerativeClustering(n_clusters=<best k value>, linkage='ward')
 #agg.fit(X_training)
+agg = AgglomerativeClustering(n_clusters=max_k, linkage='ward')
+agg.fit(X_training)
 
 #Calculate and print the Homogeneity of this agglomerative clustering
 print("Agglomerative Clustering Homogeneity Score = " + metrics.homogeneity_score(labels, agg.labels_).__str__())
